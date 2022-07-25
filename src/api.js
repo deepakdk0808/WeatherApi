d0f623415ff1a24a5300ad454950a30882a830af
dirtdk <95955656+dirtdk@users.noreply.github.com>
2022-07-25 10:30:56 +0530

    forecast added

---
+++ src/api.js
@@ -0,0 +1,17 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '4c2a0b994emsh5aa58228c56de6ap17f83djsn03e7acea9651',
+      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "cbc9e8fd5aa9a0324441e198a29492d0"
+
+
+
+ 
+
+
